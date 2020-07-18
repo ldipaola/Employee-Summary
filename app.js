721dd4784d6dc560924daf9fa708be418b07896a
Lukey <ldipaola66@gmail.com>
2020-07-18 22:47:06 +1000

    setup basic classes

--- app.js
+++ app.js
@@ -33,3 +33,5 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+const manager = new Manager("Lucas", 1, "ldipaola66@gmail.com", 244);
+console.log(manager);

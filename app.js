d4ffaab1e981d9c1ead796d5686d69d120280568
Lukey <ldipaola66@gmail.com>
2020-07-21 22:46:45 +1000

    complete async functions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
+const util = require("util");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -10,7 +11,139 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
+const manager = [];
+const engineers = [];
+const interns = [];
+
+function managerInput() {
+
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Name: ",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "ID: ",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Email: ",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Office Number: ",
+            name: "officeNumber"
+        }
+    ]);
+  }
+
+  function engineerInput() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Name: ",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "ID: ",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Email: ",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "gitHub: ",
+            name: "github"
+        }
+    ]);
+
+  }
+  function internInput() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Name: ",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "ID: ",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Email: ",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "School: ",
+            name: "school"
+        }
+    ]);
+
+  }
+
+  function askForInput () {
+     return inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "What role would you like to add?",
+            choices: ["Engineer", "Intern", "None, my team is complete"]
+          },
+    ]);
+  }
+
+//   function createEngineer (engineer) {
+//     let myEngineer= new Intern (engineer.name, engineer.id, engineer.email, engineer.github);
+//     return myEngineer;
+    
+//   }
+//   function createIntern(intern) {
+//     let myIntern= new Intern (intern.name, intern.id, intern.email, intern.school);
+//     return myIntern;
+//   }
+
+  async function employeeInput () {
+     let addEmployee = await askForInput();
+     if (addEmployee.role == "Engineer"){
+         let engineer = await engineerInput();
+         let myEngineer= new Intern (engineer.name, engineer.id, engineer.email, engineer.github);
+         engineers.push(myEngineer);
+         employeeInput();
+     } else if (addEmployee.role == "Intern"){
+         let intern = await internInput();
+         let myIntern= new Intern (intern.name, intern.id, intern.email, intern.school);
+         interns.push(myIntern);
+         employeeInput();
+     } else {
+        console.log(manager, engineers, interns);
+         return
+     }
+  }
+
+
+  managerInput()
+    .then(function(answers) {
+        let myManager = new Manager (answers.name, answers.id, answers.email, answers.officeNumber);
+        manager.push(myManager);
+        employeeInput();
+    });
+
+
+
+    
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -33,5 +166,3 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
-const manager = new Manager("Lucas", 1, "ldipaola66@gmail.com", 244);
-console.log(manager);
